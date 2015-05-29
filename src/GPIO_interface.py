9cd9039cf3290dec40386d00f5e2ed20e20946a9
Kinsey Moore <kmoore@digium.com>
2015-05-29 07:23:24 -0500

    Convert hard coded sleep logic to cancelable timers
    Add the framework and locking necessary to get the door relocking code
    out of a busywait and into a more flexible timer that can be canceled
    and rescheduled as necessary. This fixes usability issues with repeated
    attempts to unlock the door in quick succession.

--- src/GPIO_interface.py
+++ src/GPIO_interface.py
@@ -27,72 +27,107 @@ def LockDoor():
   GPIO.output(button_pin, GPIO.LOW)
   print("Door Should Now be Locked.")
 
-  
-def WaitToCloseThenLock():
-  #print("Waiting for 10 Seconds before scanning for door closure")
-  time.sleep(7)
-  #input = 0
-  reedSensor_pin = 22
-  doorStateOpen = 0  #reedSensor_pin is has a pullup resistor to 5V
-  doorStateClosed = 1 #reed switch will be connected to ground
+
+from threading import Timer, RLock
+CLOSE_TIMER = {
+  'lock': RLock(),
+  'waiting_timer': None
+}
+
+def ScheduleLockStep(lock_stage, delay=0.5, override=False):
+  with CLOSE_TIMER['lock']:
+    if CLOSE_TIMER['waiting_timer']:
+      if override:
+        # if there's a timer running, kill it
+        CLOSE_TIMER['waiting_timer'].cancel()
+        CLOSE_TIMER['waiting_timer'] = None
+      else:
+        # not overriding, bail
+        return
+    CLOSE_TIMER['waiting_timer'] = Timer(delay, lock_stage)
+    CLOSE_TIMER['waiting_timer'].start()
+
+REED_SENSOR_PIN = 22
+DOOR_STATE_OPEN = 0  #REED_SENSOR_PIN is has a pullup resistor to 5V
+DOOR_STATE_CLOSED = 1 #reed switch will be connected to ground
+BUTTON_STATE_PRESSED = 0
+BUTTON_STATE_RELEASED = 1
+UNLOCK_BUTTON_PIN = 27
+
+
+def CleanTimer():
+  with CLOSE_TIMER['lock']:
+    CLOSE_TIMER['waiting_timer'] = None
+
+def AttemptLock():
+  '''Door lock attempt initiation routine'''
+  CleanTimer()
+  # do work which may include scheduling another timer
   #GPIO.setwarnings(False)
   GPIO.setmode(GPIO.BCM)
-  GPIO.setup(reedSensor_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-  #input = GPIO.input(reedSensor_pin)
-  #print(input) 
-  print("Awaiting Door To Close")
+  GPIO.setup(REED_SENSOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-  while True:
-    input = GPIO.input(reedSensor_pin)
-    print(input)
-    if (input == doorStateClosed):
-      #Check For Debounce
-      print("DoorClosedDetected:  Checking for Debounce")
-      time.sleep(0.5)
-      input = GPIO.input(reedSensor_pin)
-      if (input == doorStateOpen):
-        print("Debounce Failed")
-        continue
-      else:
-        print("Door-Closed-Sensor Debounce Test Passed")
-        time.sleep(3)
-        input = GPIO.input(reedSensor_pin)
-        if (input == doorStateClosed):
-          print("Door Closed for 3 Seconds, Locking Door")
-          #Lock Door
-	  LockDoor()
-          break
-        else:
-          print("Door No Longer Closed, Continuing to wait for door closure.")
-          continue
+  input = GPIO.input(REED_SENSOR_PIN)
+  print("Waiting for door to close, input: %s" % input)
+  if (input != DOOR_STATE_CLOSED):
+    # Door not closed, reschedule check for later
+    ScheduleLockStep(AttemptLock)
+    return
 
+  # Door appears closed, check For Debounce
+  print("DoorClosedDetected:  Checking for Debounce")
+  ScheduleLockStep(DoDebounce)
+
+def DoDebounce():
+  '''Door lock debounce check'''
+  CleanTimer()
+  input = GPIO.input(REED_SENSOR_PIN)
+  if (input == DOOR_STATE_OPEN):
+    print("Debounce Failed")
+    ScheduleLockStep(AttemptLock)
+    return
+
+  print("Door-Closed-Sensor Debounce Test Passed")
+  ScheduleLockStep(FinalDoorCheck, delay=3)
+
+def FinalDoorCheck():
+  '''Make sure someone didn't change their mind right after closing the door and reopen it'''
+  CleanTimer()
+  input = GPIO.input(REED_SENSOR_PIN)
+  if (input != DOOR_STATE_CLOSED):
+    print("Door No Longer Closed, Continuing to wait for door closure.")
+    ScheduleLockStep(AttemptLock)
+    return
+
+  print("Door Closed for 3 Seconds, Locking Door")
+  LockDoor()
+  
+def WaitToCloseThenLock():
+  #print("Waiting for 7 Seconds before scanning for door closure")
+  ScheduleLockStep(AttemptLock, delay=7, override=True)
 
 def PrintReedSwitchState():
-  reedSensor_pin = 22
   GPIO.setmode(GPIO.BCM)
-  GPIO.setup(reedSensor_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+  GPIO.setup(REED_SENSOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
   while True:
-    input = GPIO.input(reedSensor_pin)
+    input = GPIO.input(REED_SENSOR_PIN)
     print(input)
 
 
 def WaitForButtonUnlockThenLock():
-  buttonPressedState = 0
-  buttonReleasedState = 1
-  unlockButton_pin = 27
   GPIO.setmode(GPIO.BCM)
-  GPIO.setup(unlockButton_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+  GPIO.setup(UNLOCK_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
   while True:
-    myResult = GPIO.input(unlockButton_pin)
+    myResult = GPIO.input(UNLOCK_BUTTON_PIN)
     #print(myResult)
-    if (myResult == buttonPressedState):
+    if (myResult == BUTTON_STATE_PRESSED):
       #Check For Debounce
       print("Button Press Detected:  Checking for Debounce")
       time.sleep(0.5)   
-      input = GPIO.input(unlockButton_pin)
-      if (myResult == buttonReleasedState):
+      input = GPIO.input(UNLOCK_BUTTON_PIN)
+      if (myResult == BUTTON_STATE_RELEASED):
         print("Button Press Debounce Failed")
         continue
       else:
